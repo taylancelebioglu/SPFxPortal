c1d71c296762f00a5f206c42f37a5e97ae179de4
Taylan <taylancelebioglu@gmail.com>
2018-02-08 23:21:47 +0300

    Content Type and Columns have been deployed

---
+++ src/webparts/appsMenu/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAppsMenuWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AppsMenuWebPartStrings' {
+  const strings: IAppsMenuWebPartStrings;
+  export = strings;
+}
